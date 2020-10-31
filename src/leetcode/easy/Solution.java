d043dd7a33e237bae0031729895b4dfe7c73bf6f
Aakash Shah <aakashshahd@gmail.com>
2020-10-31 14:09:18 -0400

    Update

--- src/leetcode/easy/Solution.java
+++ src/leetcode/easy/Solution.java
@@ -2,13 +2,14 @@ package leetcode.easy;
 
 public class Solution {
     public static void main(String[] args) {
-        // Define input
-        int[] intArray = new int[]{};
         switch (args[1]) {
             case "RemoveDuplicates":
             case "LeetCode26":
             case "LC26":
-                new RemoveDuplicates().removeDuplicates(intArray);
+                // Define input
+                int[] intArray = new int[]{};
+                System.out.println("Returned length of the new Array: "
+                                       + new RemoveDuplicates().removeDuplicates(intArray));
         }
     }
 }
