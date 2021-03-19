33c010eef411688d525f93c193aa43533813f2b9
Aakash Shah <aakashshahd@gmail.com>
2021-03-19 18:18:36 -0400

    94

---
+++ src/leetcode/medium/BinaryTreeInorderTraversal.java
@@ -0,0 +1,43 @@
+package leetcode.medium;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Utility.TreeNode;
+
+/**
+ * <p>Leetcode question: 94</p>
+ * <p>Link to question: https://leetcode.com/problems/binary-tree-inorder-traversal/</p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        return traverse(new ArrayList<>(), root);
+    }
+
+    public List<Integer> traverse(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return result;
+        }
+        traverse(result, root.left);
+        result.add(root.val);
+        traverse(result, root.right);
+        return result;
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
