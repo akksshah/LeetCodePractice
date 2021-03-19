afa5e1b505ae402f4635a31d64616939c198f5b2
Aakash Shah <aakashshahd@gmail.com>
2021-03-19 12:00:49 -0400

    Fix code

--- python/visualize_tree.py
+++ python/visualize_tree.py
@@ -17,16 +17,18 @@ def deserialize(string):
     root = kids.pop()
     for node in nodes:
         if node:
-            if kids: node.left = kids.pop()
-            if kids: node.right = kids.pop()
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
     return root
 
 
-def drawtree(root):
-    def height(root):
-        return 1 + max(height(root.left), height(root.right)) if root else -1
+def draw_tree(root):
+    def height(root_node):
+        return 1 + max(height(root_node.left), height(root_node.right)) if root_node else -1
 
-    def jumpto(x, y):
+    def jump_to(x, y):
         t.penup()
         t.goto(x, y)
         t.pendown()
@@ -34,23 +36,23 @@ def drawtree(root):
     def draw(node, x, y, dx):
         if node:
             t.goto(x, y)
-            jumpto(x, y - 20)
+            jump_to(x, y - 20)
             t.write(node.val, align='center', font=('Arial', 12, 'normal'))
             draw(node.left, x - dx, y - 60, dx / 2)
-            jumpto(x, y - 20)
+            jump_to(x, y - 20)
             draw(node.right, x + dx, y - 60, dx / 2)
 
     import turtle
     t = turtle.Turtle()
-    t.speed(0);
+    t.speed(0)
     turtle.delay(0)
     h = height(root)
-    jumpto(0, 30 * h)
+    jump_to(0, 30 * h)
     draw(root, 0, 30 * h, 40 * h)
     t.hideturtle()
     turtle.mainloop()
 
 
 if __name__ == '__main__':
-    drawtree(deserialize('[1,2,3,null,null,4,null,null,5]'))
-    #drawtree(deserialize('[2,1,3,0,7,9,1,2,null,1,0,null,null,8,8,null,null,null,null,7]'))
+    draw_tree(deserialize('[1,2,3,null,null,4,null,null,5]'))
+    # draw_tree(deserialize('[2,1,3,0,7,9,1,2,null,1,0,null,null,8,8,null,null,null,null,7]'))
