4039d57ab8626ad8743e4e6c91143f5e594ecb0a
Aakash Shah <aakashshahd@gmail.com>
2021-03-19 19:44:00 -0400

    20

---
+++ src/leetcode/easy/ValidParentheses.java
@@ -0,0 +1,58 @@
+package leetcode.easy;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * <p>Leetcode question: 20</p>
+ * <p>Link to question: https://leetcode.com/problems/valid-parentheses/</p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    Character c = stack.pop();
+                    if (c != ch) {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    @Test
+    public void test1() {
+        assertTrue(new ValidParentheses().isValid("()"));
+    }
+
+    @Test
+    public void test2() {
+        assertFalse(new ValidParentheses().isValid("([)]"));
+    }
+}
