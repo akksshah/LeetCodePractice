22fc704f2396b388c7764518755acdadca5aa835
Aakash Shah <aakashshahd@gmail.com>
2021-03-21 16:47:38 -0400

    350

---
+++ src/leetcode/easy/IntersectionOfTwoArrays2.java
@@ -0,0 +1,52 @@
+package leetcode.easy;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * <p>Leetcode question: 350</p>
+ * <p>Link to question: https://leetcode.com/problems/intersection-of-two-arrays-ii/description/</p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class IntersectionOfTwoArrays2 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                result.add(num);
+                if (map.get(num) == 1) {
+                    map.remove(num);
+                } else {
+                    map.put(num, map.get(num) - 1);
+                }
+
+            }
+        }
+        return result.stream().mapToInt(integer -> integer).toArray();
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
