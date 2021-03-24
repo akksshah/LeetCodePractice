3635557ad1eb85ce52b4c23cd02d74e1a3ccebcd
Aakash Shah <aakashshahd@gmail.com>
2021-03-24 12:21:01 -0400

    141

---
+++ src/leetcode/easy/LinkedListCycle.java
@@ -0,0 +1,61 @@
+package leetcode.easy;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import utility.TreeNode;
+import utility.ArrayAsString;
+import utility.ListNode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * <p>Leetcode question: 141</p>
+ * <p>Link to question: https://leetcode.com/problems/linked-list-cycle/</p>
+ * <p>Link to solution: solution 2</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+
+    public boolean hasCycle2(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
