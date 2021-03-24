4525906882f08c0e7475c67e9c72d910ffc8b5ba
Aakash Shah <aakashshahd@gmail.com>
2021-03-24 12:04:37 -0400

    1281

---
+++ src/leetcode/easy/SubtractTheProductAndSumOfDigitsOfAnInteger.java
@@ -0,0 +1,30 @@
+package leetcode.easy;import org.junit.Test;
+
+import utility.TreeNode;
+import utility.ArrayAsString;
+import utility.ListNode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+/**
+ * <p>Leetcode question: </p>
+ * <p>Link to question: </p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class SubtractTheProductAndSumOfDigitsOfAnInteger {
+
+
+
+    @Test
+    public void test1() {
+    
+    }
+    
+    @Test
+    public void test2() {
+    
+    }
+}
