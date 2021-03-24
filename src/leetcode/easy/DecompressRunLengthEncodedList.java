c0b2909091246717a9f2854fe50005f805bf595f
Aakash Shah <aakashshahd@gmail.com>
2021-03-24 12:01:42 -0400

    1313

---
+++ src/leetcode/easy/DecompressRunLengthEncodedList.java
@@ -0,0 +1,43 @@
+package leetcode.easy;
+
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+import utility.TreeNode;
+import utility.ArrayAsString;
+import utility.ListNode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * <p>Leetcode question: 1313</p>
+ * <p>Link to question: https://leetcode.com/problems/decompress-run-length-encoded-list/</p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class DecompressRunLengthEncodedList {
+    public int[] decompressRLElist(int[] nums) {
+        int s = IntStream.iterate(0, i -> i < nums.length, i -> i + 2).map(i -> nums[i]).sum();
+        int[] res = new int[s];
+        for (int i = 0, j = 0 ; i < nums.length ; i += 2) {
+            while (nums[i]-- > 0) {
+                res[j++] = nums[i + 1];
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
