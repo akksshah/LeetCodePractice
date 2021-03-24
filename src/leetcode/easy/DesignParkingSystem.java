eaef268fe280ce4d2e1bec7a34d583e450396117
Aakash Shah <aakashshahd@gmail.com>
2021-03-24 12:13:49 -0400

    1603

--- src/leetcode/easy/DesignParkingSystem.java
+++ src/leetcode/easy/DesignParkingSystem.java
@@ -1,4 +1,6 @@
-package leetcode.easy;import org.junit.Test;
+package leetcode.easy;
+
+import org.junit.Test;
 
 import utility.TreeNode;
 import utility.ArrayAsString;
@@ -8,23 +10,48 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+
 /**
- * <p>Leetcode question: </p>
- * <p>Link to question: </p>
+ * <p>Leetcode question: 1603</p>
+ * <p>Link to question: https://leetcode.com/problems/design-parking-system/</p>
  * <p>Link to solution: My solution</p>
  * <p>Link to preferred solution: </p>
  */
 public class DesignParkingSystem {
+    public class ParkingSystem {
+        private int big;
+        private int small;
+        private int medium;
 
+        public ParkingSystem(int big, int medium, int small) {
+            this.big = big;
+            this.medium = medium;
+            this.small = small;
+        }
 
+        public boolean addCar(int carType) {
+            return switch (carType) {
+                case 1 -> big-- > 0;
+                case 2 -> medium-- > 0;
+                case 3 -> small-- > 0;
+                default -> false;
+            };
+        }
+    }
+
+    /**
+     * Your ParkingSystem object will be instantiated and called as such:
+     * ParkingSystem obj = new ParkingSystem(big, medium, small);
+     * boolean param_1 = obj.addCar(carType);
+     */
 
     @Test
     public void test1() {
-    
+
     }
-    
+
     @Test
     public void test2() {
-    
+
     }
 }
