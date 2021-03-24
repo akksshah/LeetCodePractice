a28cfc416c0ef3e6a9813fe1fc40ff917e8f8dbb
Aakash Shah <aakashshahd@gmail.com>
2021-03-24 14:52:20 -0400

    1108

---
+++ src/leetcode/easy/DefangingAnIpAddress.java
@@ -0,0 +1,35 @@
+package leetcode.easy;
+
+import org.junit.Test;
+
+import utility.TreeNode;
+import utility.ArrayAsString;
+import utility.ListNode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * <p>Leetcode question: 1108</p>
+ * <p>Link to question: https://leetcode
+ * .com/problems/defanging-an-ip-address/</p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class DefangingAnIpAddress {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
