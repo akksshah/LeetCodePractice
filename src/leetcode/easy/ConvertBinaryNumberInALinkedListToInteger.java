f20b42752181ad63c626794bd13cafff4d4d22e9
Aakash Shah <aakashshahd@gmail.com>
2021-03-24 14:19:59 -0400

    1290

---
+++ src/leetcode/easy/ConvertBinaryNumberInALinkedListToInteger.java
@@ -0,0 +1,41 @@
+package leetcode.easy;
+
+import org.junit.Test;
+
+import utility.TreeNode;
+import utility.ArrayAsString;
+import utility.ListNode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * <p>Leetcode question: 1290</p>
+ * <p>Link to question: https://leetcode
+ * .com/problems/convert-binary-number-in-a-linked-list-to-integer/</p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class ConvertBinaryNumberInALinkedListToInteger {
+    public int getDecimalValue(ListNode head) {
+        int result = 0;
+        while (head != null) {
+            result = result << 1;
+            result += head.val;
+            head = head.next;
+        }
+        return result;
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
