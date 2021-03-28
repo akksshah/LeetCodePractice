96e1dd54c44a0e3e358015bbaabb9953980705df
Aakash Shah <aakashshahd@gmail.com>
2021-03-28 03:20:13 -0400

    26

---
+++ src/leetcode/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,41 @@
+package leetcode.easy;
+
+import org.junit.Test;
+
+import utility.TreeNode;
+import utility.ArrayAsString;
+import utility.ListNode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * <p>Leetcode question: 26</p>
+ * <p>Link to question: https://leetcode
+ * .com/problems/remove-duplicates-from-sorted-array/</p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1 ; j < nums.length ; j++) {
+            if (nums[j] != nums[i]) {
+                nums[++i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
