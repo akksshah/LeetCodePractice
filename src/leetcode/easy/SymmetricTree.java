258e71181e719e3d8d29984c10aa17be2ecdd0c7
Aakash Shah <aakashshahd@gmail.com>
2021-03-28 02:49:00 -0400

    101

---
+++ src/leetcode/easy/SymmetricTree.java
@@ -0,0 +1,46 @@
+package leetcode.easy;
+
+import org.junit.Test;
+
+import utility.TreeNode;
+import utility.ArrayAsString;
+import utility.ListNode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * <p>Leetcode question: 101</p>
+ * <p>Link to question: https://leetcode.com/problems/symmetric-tree/</p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        else return helper(root.left, root.right);
+    }
+    private boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
+            return false;
+        } else {
+            return left.val == right.val && helper(left.right, right.left) && helper(left.left, right.right);
+        }
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
