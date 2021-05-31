cb0879e4ea5d58fd5bbf104e70aa6c159808c476
Aakash Shah <aakashshahd@gmail.com>
2021-05-31 12:31:41 -0400

    1010
    Signed-off-by: Aakash Shah <aakashshahd@gmail.com>

---
+++ src/leetcode/medium/PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -0,0 +1,33 @@
+package leetcode.medium;
+
+import org.junit.Test;
+
+/**
+ * <p>Leetcode question: </p>
+ * <p>Link to question: </p>
+ * <p>Link to solution: My solution</p>
+ * <p>Link to preferred solution: </p>
+ */
+public class PairsOfSongsWithTotalDurationsDivisibleBy60 {
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] remainders = new int[60];
+        int count = 0;
+        for (int t : time) {
+            // check if a%60==0 && b%60==0
+            // check if a%60+b%60==60
+            count += t % 60 == 0 ? remainders[0] : remainders[60 - t % 60];
+            remainders[t % 60]++; // remember to update the remainders
+        }
+        return count;
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
