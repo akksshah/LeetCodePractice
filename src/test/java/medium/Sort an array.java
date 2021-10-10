45404f35db3c5d7bc5222d71361c8dbf219c2feb
HariChandana <80469143+HariChandana4902@users.noreply.github.com>
2021-10-10 19:50:48 +0530

    Sort an array question in leetcode

---
+++ src/test/java/medium/Sort an array.java
@@ -0,0 +1,21 @@
+class Solution {
+  public int[] sortArray(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            for(int j = i+1; j > 0; j--)
+            {
+                if(arr[j-1]>arr[j])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+                else{
+                    
+                    break;
+                }
+            }
+        }
+      return arr;
+    }
+}
